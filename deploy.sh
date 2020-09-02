0bc94e52753f09c3fbbd70ea44c3472320201ca5
auino <enrico.cambiaso@gmail.com>
2020-09-02 18:17:51 +0200

    Minor enhancements to the code

--- deploy.sh
+++ deploy.sh
@@ -11,3 +11,7 @@ git push -u origin master
 
 python3 setup.py sdist bdist_wheel
 python3 -m twine upload dist/*
+
+rm -rf build 2> /dev/null
+rm -rf dist 2> /dev/null
+rm -rf thestockslib.egg-info 2> /dev/null
